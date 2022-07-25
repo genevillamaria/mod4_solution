f309fc63aa0eba0f40b0c96ff8bebe64773722a0
genevillamaria <59555551+genevillamaria@users.noreply.github.com>
2022-07-25 13:48:02 -0700

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,6 +1,6 @@
 (function (window) {
 	var byeSpeaker = {};
-	var speakWord = "Good Bye";
+	var speakWord = "Goodbye";
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
